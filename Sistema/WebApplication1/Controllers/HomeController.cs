e7fd01655f9b454f60e25b0dde1685f65b2d6174
Henrique dos Santos <hsantosdasilva25@gmail.com>
2024-04-16 18:09:38 -0400

    branch correta do envio do crud profissoes

---
+++ Sistema/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using app.BE;
+using app.Data;
+using app.DTO;
+
+namespace app.Controllers
+{
+    public class ProfissoesController : Controller
+    {
+        private ProfissoesBE _be;
+        private AppDbContext _context;
+
+        public ProfissoesController(ProfissoesBE be, AppDbContext context)
+        {
+            _be = be;
+            _context = context;
+        }
+
+        // GET: Profissoes
+        [Route("getAllProfissoes")]
+        [HttpGet]
+        public async Task<IActionResult> GetAll(ProfissoesDTO dto)
+        {
+            try
+            {
+                var response = await _be.GetAll(dto);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST: Profissoes
+        [Route("insertProfissoes")]
+        [HttpPost]
+        public async Task<IActionResult> Insert([FromBody] ProfissoesDTO profissoes)
+        {
+            try
+            {
+                _context.BeginTransaction();
+                var response = await _be.Insert(profissoes);
+                _context.Commit();
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                _context.Rollback();
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PATCH: Profissoes
+        [Route("updateProfissoes")]
+        [HttpPatch]
+        public async Task<IActionResult> Update([FromBody] ProfissoesDTO profissoes)
+        {
+            try
+            {
+                _context.BeginTransaction();
+                var response = await _be.Update(profissoes);
+                _context.Commit();
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                _context.Rollback();
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE: Profissoes
+        [Route("deleteProfissoes")]
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromQuery] int id)
+        {
+            try
+            {
+                _context.BeginTransaction();
+                await _be.Delete(id);
+                _context.Commit();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _context.Rollback();
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
