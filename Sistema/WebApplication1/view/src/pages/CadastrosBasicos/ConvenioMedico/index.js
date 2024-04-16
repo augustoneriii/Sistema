01129c7487ef23f1557c09174fd9935bea1aa040
Augusto De Oliveira Neri <augustodeo.neri@gmail.com>
2024-04-16 13:35:44 -0400

    pequenas alterações

--- Sistema/WebApplication1/view/src/pages/CadastrosBasicos/ConvenioMedico/index.js
+++ Sistema/WebApplication1/view/src/pages/CadastrosBasicos/ConvenioMedico/index.js
@@ -8,207 +8,230 @@ import { Column } from 'primereact/column';
 import { ConvenioService } from './service/ConvenioService.js';
 import { Toast } from 'primereact/toast';
 import { Dialog } from 'primereact/dialog';
+import { SplitButton } from 'primereact/splitbutton';
+
 
 function ConvenioMedico() {
-  // const { convenioVisible, setConvenioVisible } = useContext(SidebarContext);
-  const [convenioVisible, setConvenioVisible] = useState(true);
+    // const { convenioVisible, setConvenioVisible } = useContext(SidebarContext);
+    const [convenioVisible, setConvenioVisible] = useState(true);
 
-  const emptyConvenio = {
-    id: null,
-    nome: '',
-    telefone: '',
-    email: '',
-    site: ''
-  };
+    const emptyConvenio = {
+        id: null,
+        nome: '',
+        telefone: '',
+        email: '',
+        site: ''
+    };
 
-  const [convenios, setConvenios] = useState([]);
-  const [convenioDialog, setConvenioDialog] = useState(false);
-  const [deleteConvenioDialog, setDeleteConvenioDialog] = useState(false);
-  const [convenio, setConvenio] = useState(emptyConvenio);
-  const [selectedConvenios, setSelectedConvenios] = useState(null);
-  const [submitted, setSubmitted] = useState(false);
-  const [globalFilter, setGlobalFilter] = useState(null);
-  const toast = useRef(null);
+    const [convenios, setConvenios] = useState([]);
+    const [convenioDialog, setConvenioDialog] = useState(false);
+    const [deleteConvenioDialog, setDeleteConvenioDialog] = useState(false);
+    const [convenio, setConvenio] = useState(emptyConvenio);
+    const [selectedConvenios, setSelectedConvenios] = useState(null);
+    const [submitted, setSubmitted] = useState(false);
+    const [globalFilter, setGlobalFilter] = useState(null);
+    const toast = useRef(null);
 
-  const dt = useRef(null);
+    const dt = useRef(null);
 
 
-  useEffect(() => {
-    const currentToken = localStorage.getItem('token') || '';
-    ConvenioService.getConvenios(currentToken)
-      .then(response => {
-        setConvenios(response.data); // Acesso à array de convenios
-      })
-      .catch(error => {
-        console.error("Erro ao buscar convenios:", error);
-      });
-  }, []);
-
-  const openNew = () => {
-    setConvenio(emptyConvenio);
-    setSubmitted(false);
-    setConvenioDialog(true);
-  };
-
-  const hideDeleteConvenioDialog = () => {
-    setDeleteConvenioDialog(false);
-  };
-
-  const saveConvenio = () => {
-    setSubmitted(true);
-
-    if (convenio.nome.trim()) {
-        let _convenios = [...convenios];
-        let _convenio = { ...convenio };
-
+    useEffect(() => {
         const currentToken = localStorage.getItem('token') || '';
-        if (convenio.id) {
-            const index = findIndexById(convenio.id);
-            _convenios[index] = _convenio;
-            toast.current.show({ severity: 'success', summary: 'Sucesso', detail: 'Convenio Atualizado', life: 3000 });
-            ConvenioService.updateConvenio(_convenio);
-        } else {
-            _convenios.push(_convenio);
-            console.log("convenio", _convenio);
-            ConvenioService.createConvenio(_convenio);
-            toast.current.show({ severity: 'success', summary: 'Sucesso', detail: 'Convenio Criado', life: 3000 });
-        }
+        ConvenioService.getConvenios(currentToken)
+            .then(response => {
+                setConvenios(response.data); // Acesso à array de convenios
+            })
+            .catch(error => {
+                console.error("Erro ao buscar convenios:", error);
+            });
+    }, []);
 
-        setConvenios(_convenios);
-        setConvenioDialog(false);
+    const openNew = () => {
         setConvenio(emptyConvenio);
-    }
-};
+        setSubmitted(false);
+        setConvenioDialog(true);
+    };
+
+    const hideDeleteConvenioDialog = () => {
+        setDeleteConvenioDialog(false);
+    };
+
+    const saveConvenio = () => {
+        setSubmitted(true);
+
+        if (convenio.nome.trim()) {
+            let _convenios = [...convenios];
+            let _convenio = { ...convenio };
+
+            const currentToken = localStorage.getItem('token') || '';
+            if (convenio.id) {
+                const index = findIndexById(convenio.id);
+                _convenios[index] = _convenio;
+                toast.current.show({ severity: 'success', summary: 'Sucesso', detail: 'Convenio Atualizado', life: 3000 });
+                ConvenioService.updateConvenio(_convenio);
+            } else {
+                _convenios.push(_convenio);
+                console.log("convenio", _convenio);
+                ConvenioService.createConvenio(_convenio);
+                toast.current.show({ severity: 'success', summary: 'Sucesso', detail: 'Convenio Criado', life: 3000 });
+            }
+
+            setConvenios(_convenios);
+            setConvenioDialog(false);
+            setConvenio(emptyConvenio);
+        }
+    };
 
 
-  const editConvenio = (convenio) => {
-    setConvenio({ ...convenio });
-    setConvenioDialog(true);
-  };
+    const editConvenio = (convenio) => {
+        setConvenio({ ...convenio });
+        setConvenioDialog(true);
+    };
 
-  const confirmDeleteConvenio = (convenio) => {
-    setConvenio(convenio);
-    setDeleteConvenioDialog(true);
-  };
+    const confirmDeleteConvenio = (convenio) => {
+        setConvenio(convenio);
+        setDeleteConvenioDialog(true);
+    };
 
-  const deleteConvenio = () => {
-    const currentToken = localStorage.getItem('token') || '';
+    const deleteConvenio = () => {
+        const currentToken = localStorage.getItem('token') || '';
 
-    if (convenio && convenio.id) {
+        if (convenio && convenio.id) {
 
-      ConvenioService.deleteConvenio(convenio.id, currentToken)
-        .then(() => {
-          toast.current.show({ severity: 'success', summary: 'Sucesso', detail: 'Convenio Deletado', life: 3000 });
-          setConvenios(convenios.filter(val => val.id !== convenio.id));
-          setDeleteConvenioDialog(false);
-          setConvenio(emptyConvenio);
-        })
-        .catch(error => {
-          toast.current.show({ severity: 'Error', summary: 'Error', detail: `Erro ao deletar convenio: ${error}`, life: 3000 });
-          console.error("Erro ao deletar convenio:", error);
-        });
-    } else {
-      console.error("Erro: id do convenio é undefined");
-    }
-  };
+            ConvenioService.deleteConvenio(convenio.id, currentToken)
+                .then(() => {
+                    toast.current.show({ severity: 'success', summary: 'Sucesso', detail: 'Convenio Deletado', life: 3000 });
+                    setConvenios(convenios.filter(val => val.id !== convenio.id));
+                    setDeleteConvenioDialog(false);
+                    setConvenio(emptyConvenio);
+                })
+                .catch(error => {
+                    toast.current.show({ severity: 'Error', summary: 'Error', detail: `Erro ao deletar convenio: ${error}`, life: 3000 });
+                    console.error("Erro ao deletar convenio:", error);
+                });
+        } else {
+            console.error("Erro: id do convenio é undefined");
+        }
+    };
 
-  const findIndexById = (id) => {
-    let index = -1;
-    for (let i = 0; i < convenios.length; i++) {
-      if (convenios[i].id === id) {
-        index = i;
-        break;
-      }
-    }
-    return index;
-  };
+    const findIndexById = (id) => {
+        let index = -1;
+        for (let i = 0; i < convenios.length; i++) {
+            if (convenios[i].id === id) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    };
 
 
-  const onInputChange = (e, name) => {
-    const val = (e.target && e.target.value) || '';
-    let _convenio = { ...convenio };
-    _convenio[`${name}`] = val;
-    setConvenio(_convenio);
-  };
+    const onInputChange = (e, name) => {
+        const val = (e.target && e.target.value) || '';
+        let _convenio = { ...convenio };
+        _convenio[`${name}`] = val;
+        setConvenio(_convenio);
+    };
 
-  const leftToolbarTemplate = () => {
-    return (
-      <React.Fragment>
-        <Button label="Novo" icon="pi pi-plus" className="border-round p-button-success mr-2" onClick={openNew} />
-      </React.Fragment>
+    const leftToolbarTemplate = () => {
+        return (
+            <React.Fragment>
+                <Button label="Novo" icon="pi pi-plus" className="border-round p-button-success mr-2" onClick={openNew} />
+            </React.Fragment>
+        );
+    };
+
+    const actionsButton = (rowData) => {
+        return [
+            {
+                label: 'Editar', icon: 'pi pi-pencil', command: () => { editConvenio(rowData) }
+            },
+            {
+                label: 'Excluir', icon: 'pi pi-trash', command: () => { confirmDeleteConvenio(rowData) }
+            }
+        ];
+    };
+
+
+    const actionButtonTemplate = (rowData) => {
+        return (
+            <React.Fragment>
+                <SplitButton icon="pi pi-bars" model={actionsButton(rowData)} className="border-round" />
+            </React.Fragment>
+        );
+    };
+
+    const actionBodyTemplate = (rowData) => {
+        return (
+            <React.Fragment>
+                <Button icon="pi pi-pencil" className="border-round p-button-rounded p-button-primary" onClick={() => editConvenio(rowData)} />
+                <Button icon="pi pi-trash" className="border-round p-button-rounded p-button-danger" onClick={() => confirmDeleteConvenio(rowData)} />
+            </React.Fragment>
+        );
+    };
+
+    const header = (
+        <h1>Convenios Médicos</h1>
     );
-  };
 
-  const actionBodyTemplate = (rowData) => {
-    return (
-      <React.Fragment>
-        <Button icon="pi pi-trash" className="border-round p-button-rounded p-button-danger" onClick={() => confirmDeleteConvenio(rowData)} />
-      </React.Fragment>
+    const deleteConvenioDialogFooter = (
+        <React.Fragment>
+            <Button label="Não" icon="pi pi-times" className="border-round p-button-text" onClick={hideDeleteConvenioDialog} />
+            <Button label="Sim" icon="pi pi-check" className="border-round p-button-text" onClick={deleteConvenio} />
+        </React.Fragment>
     );
-  };
 
-  const header = (
-    <h1>Convenios Médicos</h1>
-  );
+    return (
+        <>
+            <Toast ref={toast} />
 
-  const deleteConvenioDialogFooter = (
-    <React.Fragment>
-      <Button label="Não" icon="pi pi-times" className="border-round p-button-text" onClick={hideDeleteConvenioDialog} />
-      <Button label="Sim" icon="pi pi-check" className="border-round p-button-text" onClick={deleteConvenio} />
-    </React.Fragment>
-  );
+            <Dialog header={header} modal={false} visible={convenioVisible} style={{ width: '80vw', height: '80vh' }} onHide={() => setConvenioVisible(false)}>
+                <div className='card'>
+                    <div className='grid'>
+                        <div className="field col-6">
+                            <label htmlFor="nome">Nome</label>
+                            <InputText className='w-full' id="nome" value={convenio.nome} onChange={(e) => onInputChange(e, 'nome')} />
+                        </div>
+                        <div className="field col-6">
+                            <label htmlFor="telefone">Telefone</label>
+                            <InputText className='w-full' id="telefone" value={convenio.telefone} onChange={(e) => onInputChange(e, 'telefone')} />
+                        </div>
+                        <div className="field col-6">
+                            <label htmlFor="email">E-Mail</label>
+                            <InputText className='w-full' id="email" value={convenio.email} onChange={(e) => onInputChange(e, 'email')} />
+                        </div>
+                        <div className="field col-6">
+                            <label htmlFor="site">Site</label>
+                            <InputText className='w-full' id="site" value={convenio.site} onChange={(e) => onInputChange(e, 'site')} />
+                        </div>
+                        <div className="field col">
+                            <Button label="Salvar" icon="pi pi-check" className="border-round p-button-text" onClick={saveConvenio} />
+                        </div>
+                    </div>
+                </div>
 
-  return (
-    <>
-      <Toast ref={toast} />
+                <div className="card">
+                    <DataTable ref={dt} value={convenios} selection={selectedConvenios} onSelectionChange={e => setSelectedConvenios(e.value)}
+                        dataKey="id" paginator rows={10} rowsPerPageOptions={[5, 10, 25]} scrollable scrollHeight="200px"
+                        paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+                        currentPageReportTemplate="Mostrando {first} a {last} de {totalRecords} convenios" globalFilter={globalFilter}>
+                        <Column body={actionButtonTemplate}></Column>
+                        <Column field="nome" header="Nome" sortable></Column>
+                        <Column field="telefone" header="Telefone" sortable></Column>
+                        <Column field="email" header="E-Mail" sortable></Column>
+                        <Column field="site" header="Site" sortable></Column>
+                    </DataTable>
+                </div>
 
-      <Dialog header={header} modal={false}  visible={convenioVisible} style={{ width: '80vw', height:'80vh' }} onHide={() => setConvenioVisible(false)}>
-        <div className='card'>
-          <div className='grid'>
-            <div className="field col-6">
-              <label htmlFor="nome">Nome</label>
-              <InputText className='w-full' id="nome" value={convenio.nome} onChange={(e) => onInputChange(e, 'nome')} />
-            </div>
-            <div className="field col-6">
-              <label htmlFor="telefone">Telefone</label>
-              <InputText className='w-full' id="telefone" value={convenio.telefone} onChange={(e) => onInputChange(e, 'telefone')} />
-            </div>
-            <div className="field col-6">
-              <label htmlFor="email">E-Mail</label>
-              <InputText className='w-full' id="email" value={convenio.email} onChange={(e) => onInputChange(e, 'email')} />
-            </div>
-            <div className="field col-6">
-              <label htmlFor="site">Site</label>
-              <InputText className='w-full' id="site" value={convenio.site} onChange={(e) => onInputChange(e, 'site')} />
-            </div>
-            <div className="field col">
-              <Button label="Salvar" icon="pi pi-check" className="border-round p-button-text" onClick={saveConvenio} />
-            </div>
-          </div>
-        </div>
-
-        <div className="card">
-          <DataTable ref={dt} value={convenios} selection={selectedConvenios} onSelectionChange={e => setSelectedConvenios(e.value)}
-            dataKey="id" paginator rows={10} rowsPerPageOptions={[5, 10, 25]} scrollable scrollHeight="200px" 
-            paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-            currentPageReportTemplate="Mostrando {first} a {last} de {totalRecords} convenios" globalFilter={globalFilter}>
-            <Column field="nome" header="Nome" sortable></Column>
-            <Column field="telefone" header="Telefone" sortable></Column>
-            <Column field="email" header="E-Mail" sortable></Column>
-            <Column field="site" header="Site" sortable></Column>
-            <Column body={actionBodyTemplate}></Column>
-          </DataTable>
-        </div>
-
-        <Dialog visible={deleteConvenioDialog} style={{ width: '450px' }} header="Confirmação" modal footer={deleteConvenioDialogFooter} onHide={hideDeleteConvenioDialog}>
-          <div className="confirmation-content">
-            <i className="pi pi-exclamation-triangle mr-2" style={{ fontSize: '2rem' }} />
-            {convenio && <span>Tem certeza que deseja excluir o convenio <b>{convenio.nome}</b>?</span>}
-          </div>
-        </Dialog>
-      </Dialog>
-    </>
-  )
+                <Dialog visible={deleteConvenioDialog} style={{ width: '450px' }} header="Confirmação" modal footer={deleteConvenioDialogFooter} onHide={hideDeleteConvenioDialog}>
+                    <div className="confirmation-content">
+                        <i className="pi pi-exclamation-triangle mr-2" style={{ fontSize: '2rem' }} />
+                        {convenio && <span>Tem certeza que deseja excluir o convenio <b>{convenio.nome}</b>?</span>}
+                    </div>
+                </Dialog>
+            </Dialog>
+        </>
+    )
 }
 
 export default ConvenioMedico
