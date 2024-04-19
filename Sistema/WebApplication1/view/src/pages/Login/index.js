0194bd2498ef955b2d890e5a4f9729c4a637fe8a
Augusto De Oliveira Neri <augustodeo.neri@gmail.com>
2024-04-19 10:56:51 -0400

    add logout

--- Sistema/WebApplication1/view/src/pages/Login/index.js
+++ Sistema/WebApplication1/view/src/pages/Login/index.js
@@ -4,6 +4,7 @@ import React from 'react'
 //components
 import { InputText } from 'primereact/inputtext'
 import { Button } from 'primereact/button'
+import { Checkbox } from 'primereact/checkbox'
 //hooks
 import { useContext, useState } from 'react'
 //context
@@ -14,53 +15,48 @@ import Logo from '../../assets/img/logoSysclin.png'
 
 function Login() {
 
-  const [user, setUser] = useState({})
-  const { login } = useContext(Context)
-  const [token] = useState(localStorage.getItem('token') || '')
-  const navigate = useNavigate()
+    const [user, setUser] = useState({})
+    const { login } = useContext(Context)
+    const [token] = useState(localStorage.getItem('token') || '')
+    const navigate = useNavigate()
 
+    if (token) {
+        navigate('/home')
+    }
 
-  if (token) {
-    navigate('/home')
-  }
+    function handleChange(e) {
+        setUser({ ...user, [e.target.name]: e.target.value })
+    }
 
-  function handleChange(e) {
-    setUser({ ...user, [e.target.name]: e.target.value })
-  }
+    function handleSubmit(e) {
+        e.preventDefault()
+        login(user)
+    }
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    login(user)
-  }
+    return (
+        <div style={{ height: '100vh' }} className="login-container flex align-items-center justify-content-center">
+            <div className="surface-card p-4 shadow-2 border-round  lg:w-6">
+                <div className="text-center ">
+                    <img src={Logo} alt="hyper" height={200} className="mb-3" />
+                </div>
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="email" className="block text-900 font-medium mb-2">Email</label>
+                        <InputText id="email" name='email' onChange={handleChange} type="text" placeholder="Email address" className="w-full mb-3" />
 
-  return (
-    <div style={{ height: '100vh' }} className="login-container flex align-items-center justify-content-center">
-      <div className="surface-card p-4 shadow-2 border-round  lg:w-6">
-        <div className="text-center ">
-          <img src={Logo} alt="hyper" height={200} className="mb-3" />
-        </div>
-        <div>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="email" className="block text-900 font-medium mb-2">Email</label>
-            <InputText id="email" name='email' onChange={handleChange} type="text" placeholder="Email address" className="w-full mb-3" />
+                        <label htmlFor="password" className="block text-900 font-medium mb-2">Password</label>
+                        <InputText id="password" name='password' type="password" onChange={handleChange} placeholder="Password" className="w-full mb-3" />
 
-            <label htmlFor="password" className="block text-900 font-medium mb-2">Password</label>
-            <InputText id="password" name='password' type="password" onChange={handleChange} placeholder="Password" className="w-full mb-3" />
-
-            <div className="flex align-items-center justify-content-between mb-6">
-              <div className="flex align-items-center">
-                {/* <Checkbox id="rememberme" onChange={e => setChecked(e.checked)} checked={checked} className="mr-2" /> */}
-                {/* <label htmlFor="rememberme">Remember me</label> */}
-              </div>
-              <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot your password?</a>
+                        <div className="flex align-items-center justify-content-between mb-6">
+                            <div className="flex align-items-center">
+                            </div>
+                        </div>
+                        <Button type="submit" label="Sign In" icon="pi pi-user" className="w-full" />
+                    </form>
+                </div>
             </div>
-
-            <Button type="submit" label="Sign In" icon="pi pi-user" className="w-full" />
-          </form>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Login
