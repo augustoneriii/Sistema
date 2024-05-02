be7babaf38bd383d6035ef2545dfcf8f46b3577e
Henrique dos Santos <hsantosdasilva25@gmail.com>
2024-05-02 13:31:04 -0400

    crud ProntuarioMedico

---
+++ Sistema/WebApplication1/BE/ProntuarioMedicoBE.cs
@@ -0,0 +1,40 @@
+﻿using app.Data;
+using app.DAO;
+using app.DTO;
+using app.DTO.Request;
+
+
+namespace app.BE
+{
+    public class ProntuarioMedicoBE
+    {
+        private AppDbContext _context;
+
+        public ProntuarioMedicoBE(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //gel all
+        public async Task<List<ProntuarioMedicoDTO>> GetAll(ProntuarioMedicoDTO dto)
+        {
+            var dao = new ProntuarioMedicoDAO(_context);
+            return await dao.GetAll(dto);
+        }
+
+        //insert
+        public async Task<int> Insert(ProntuarioMedicoRequest dto)
+        {
+            var dao = new ProntuarioMedicoDAO(_context);
+            return await dao.Insert(dto);
+        }
+
+        //update
+        public async Task<int> Update(ProntuarioMedicoRequest dto)
+        {
+            var dao = new ProntuarioMedicoDAO(_context);
+            return await dao.Update(dto);
+        }
+
+    }
+}
---
+++ Sistema/WebApplication1/DTO/ProntuarioMedicoDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace app.DTO
+{
+    public class ProntuarioMedicoDTO
+    {
+        public long? Id { get; set; }
+        public PacientesDTO? Pacientes { get; set; }
+        public ProfissionaisDTO? Profissionais { get; set; }
+        public string? PrescricaoMedicamentos { get; set; }
+        public string? EvolucaoPaciente { get; set; }
+        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
