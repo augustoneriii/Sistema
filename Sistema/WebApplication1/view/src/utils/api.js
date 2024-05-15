57ad000f18a1f0ccafd3f5ea440f2500ab686810
Augusto Neri <112219875+augustoneriii@users.noreply.github.com>
2024-05-15 12:46:27 -0400

    Update api.js

--- Sistema/WebApplication1/view/src/utils/api.js
+++ Sistema/WebApplication1/view/src/utils/api.js
@@ -9,6 +9,6 @@ export default axios.create({
    /*  baseURL: 'https://localhost:7217', //URL da api
      headers: {
          'Content-Type': 'application/json'
-    }*
+    }*/
     // baseURL: 'http://191.252.204.179:5000'  //URL da api
 })
